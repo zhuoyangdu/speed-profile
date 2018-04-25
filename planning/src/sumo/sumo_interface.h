299a18386e75bc31eaa3ddd87c65a053bd8f7bbf
Zhuoyang Du <zydu@zju.edu.cn>
2018-04-25 14:22:07 +0800

    Add third party.

---
+++ planning/src/sumo/sumo_interface.h
@@ -0,0 +1,13 @@
+// Copyright [2018] <Zhuoyang Du>
+
+#ifndef PLANNING_SRC_SUMO_SUMO_INTERFACE_H_
+#define PLANNING_SRC_SUMO_SUMO_INTERFACE_H_
+
+namespace planning {
+    class SumoInterface
+    {
+    public:
+        SumoInterface();
+        ~SumoInterface();
+    };
+}
